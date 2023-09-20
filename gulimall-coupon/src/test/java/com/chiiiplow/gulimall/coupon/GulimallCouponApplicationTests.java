9288818f900062edaa4d9bf8fe0ccae20d02329c
CHIIIPLOW-922 <q641484973@gmail.com>
2023-09-21 00:08:48 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/chiiiplow/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiiiplow.gulimall.coupon;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
