32618b37728563afdcb6b5acf78ecaca011f0019
CHIIIPLOW-922 <q641484973@gmail.com>
2023-09-29 02:57:26 +0800

    新建Search服务

---
+++ gulimall-search/src/main/java/com/chiiiplow/gulimall/search/constant/EsConstant.java
@@ -0,0 +1,13 @@
+package com.chiiiplow.gulimall.search.constant;
+
+/**
+ * @Description:
+ * @Created: with IntelliJ IDEA.
+ * @author: CHIIIPLOW
+ * @createTime: 2020-06-06 16:58
+ **/
+public class EsConstant {
+
+    public static final String PRODUCT_INDEX = "product";
+
+}
