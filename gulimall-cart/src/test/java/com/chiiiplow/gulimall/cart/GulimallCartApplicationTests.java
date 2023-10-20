b6e7fa4c228918d862d3ad8b55dae630a4188fda
CHIIIPLOW-922 <q641484973@gmail.com>
2023-10-20 11:02:28 +0800

    新增秒杀服务&新增购物车服务

---
+++ gulimall-cart/src/test/java/com/chiiiplow/gulimall/cart/GulimallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiiiplow.gulimall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
